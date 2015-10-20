d364bf1561233fd3a278b2f96737fa0423bc43eb
Maiclear <maidaclaro@gmail.com>
2015-10-19 22:00:31 -0300

    Agregue relacion uno a es mucho de user a commenta nivel modelo

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  has_many :comments
+
   enum role: [:admin, :editor, :user]
 
   def default_role
